163f26b81fb87b68c14067de07e6ef95d447ec6c
Sebastiano Gaudeano <sebastiano.gaudeano@live.com>
2024-03-15 17:03:02 +0100

    List view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,42 @@
+import 'dart:math';
+
+import 'package:address_24/models/person.dart';
+import 'package:address_24/services/people_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'screens/home_screen.dart';
 
 void main() {
-  runApp(SafeArea(child: HomeScreen()));
+  runApp(MaterialApp(
+    home: SafeArea(child: HomeListViewScreen()),
+  ));
+}
+
+class HomeListViewScreen extends StatelessWidget {
+  HomeListViewScreen({super.key});
+
+  final people = PeopleService().getPeople().toList();
+
+  ListTile _buildListTile(Person p) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(p.picture!.thumbnail!),
+      ),
+      title: Text(p.firstName!),
+      subtitle: Text(p.cell!),
+      trailing: Icon(Icons.favorite_border),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: people.map(_buildListTile).toList(),
+      ),
+    );
+  }
+
+  // ListTile newMethod(e) => _buildListTile(p: e);
 }
