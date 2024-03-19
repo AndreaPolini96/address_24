87d3fd581044559d01438939fdf7440fb32bd46d
Sebastiano Gaudeano <sebastiano.gaudeano@live.com>
2024-03-19 16:41:21 +0100

    base project

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'dart:math';
 
 import 'package:address_24/models/person.dart';
 import 'package:address_24/services/people_service.dart';
+import 'package:address_24/services/services.dart';
 import 'package:address_24/widgets/like_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'screens/home_screen.dart';
+import 'widgets/contact_list_item.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -32,30 +34,6 @@ class _HomeListViewScreenState extends State<HomeListViewScreen> {
 
   int _currentIndex = 0;
 
-  ListTile _buildListTile(Person p) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(p.picture!.thumbnail!),
-      ),
-      title: Text(p.firstName!),
-      subtitle: Text(p.cell!),
-      trailing: LikeButton(
-          favorite: _favorite.contains(p.id),
-          onPressed: () {
-            setState(() {
-              if (_favorite.contains(p.id)) {
-                _favorite.remove(p.id);
-              } else {
-                _favorite.add(p.id);
-              }
-
-              _favoritePeople =
-                  people.where((e) => _favorite.contains(e.id)).toList();
-            });
-          }),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,17 +52,80 @@ class _HomeListViewScreenState extends State<HomeListViewScreen> {
                 icon: Icon(Icons.favorite_rounded), label: "Favorite")
           ]),
       body: _currentIndex == 0
-          ? ListView.builder(
-              itemCount: people.length,
-              itemBuilder: (context, index) {
-                return _buildListTile(people[index]);
-              })
-          : ListView.builder(
-              itemCount: _favoritePeople.length,
-              itemBuilder: (context, index) {
-                return _buildListTile(_favoritePeople[index]);
+          ? ContactListView(
+              people: people,
+              onTileTapped: (p) {},
+              onPressed: (p) {
+                setState(() {
+                  if (_favorite.contains(p.id)) {
+                    _favorite.remove(p.id);
+                    return;
+                  }
+
+                  _favorite.add(p.id);
+                  _favoritePeople =
+                      people.where((e) => _favorite.contains(e.id)).toList();
+                });
               },
-            ),
+              isFavorite: (id) {
+                return _favorite.contains(id);
+              })
+          : ContactListView(
+              people: _favoritePeople,
+              onTileTapped: (p) {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) {
+                    return Center(
+                      child: Text("Hello new page"),
+                    );
+                  },
+                ));
+              },
+              onPressed: (p) {
+                setState(() {
+                  if (_favorite.contains(p.id)) {
+                    _favorite.remove(p.id);
+                    return;
+                  }
+
+                  _favorite.add(p.id);
+                  _favoritePeople =
+                      people.where((e) => _favorite.contains(e.id)).toList();
+                });
+              },
+              isFavorite: (id) {
+                return _favorite.contains(id);
+              }),
     );
+  }
+}
+
+class ContactListView extends StatelessWidget {
+  const ContactListView({
+    super.key,
+    required this.people,
+    required this.onPressed,
+    required this.isFavorite,
+    required this.onTileTapped,
+  });
+
+  final void Function(Person p) onPressed;
+  final bool Function(String? id) isFavorite;
+  final void Function(Person p) onTileTapped;
+
+  final List<Person> people;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          final p = people[index];
+          return ContactListItem(
+              p: p,
+              onTap: () => onTileTapped(p),
+              trailing: LikeButton(
+                  favorite: isFavorite(p.id), onPressed: () => onPressed(p)));
+        });
   }
 }
